55a6fd62d2be903fe530c2e00b605b59ac76d621
JOÃO GABRIEL COSTA FLÔR <21276663@CFP127ALUNO.LOCAL>
2022-05-17 16:59:03 -0300

    17/05

---
+++ src/main/java/com/example/consumindoapi/api/Endpoint.kt
@@ -0,0 +1,11 @@
+package com.example.consumindoapi.api;
+
+import com.google.gson.JsonObject;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path
+
+interface Endpoint {
+    @GET("/api/breed/{raca}/images/random")
+    fun getDog(@Path(value = "raca")raca : String) : Call<JsonObject>
+}
